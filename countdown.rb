1affccbd40e89d47712288b1b0b3a30301e34c7b
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-08-26 05:27:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -18,16 +18,5 @@ end
 end
 
 def countdown_with_sleep(number)
-    while number > 0
-  puts "#{number} SECOND(S)!"
-
-number -= 1
-sleep 1
-end
-if number < 1
-return "HAPPY NEW YEAR!"
-end
-    
-end
-  
+sleep 5
 end
