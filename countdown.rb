7a862b853b64584d8644d1a239f4a1341083f392
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-08-26 04:55:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+n = 12
+
+def countdown(n)
+  while n > 0
+  n -= 1
+  puts "#{n} SECOND(S)!"
+  sleep 1
+  if n = 0
+    puts "HAPPY NEW YEAR!"
+  end  
+end
+    
 end
